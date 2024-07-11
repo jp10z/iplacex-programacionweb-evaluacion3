e66f72d67e981cbde37b437fbd230bcfa4b525c3
jp10z <jpramos2910@gmail.com>
2024-07-10 21:36:56 -0400

    Se agrega estructura base y se agrega ejercicio 1

---
+++ README.md
@@ -0,0 +1,48 @@
+# IPLACEX - Programación Web - Evaluación 3
+
+Este repositorio contiene el desarrollo de la evaluación 3 de la asignatura
+`Programación web`.
+
+## Datos
+
+- Nombre: José Ramos
+- Periodo: 2024-2B
+- Asignatura: Programación Web
+
+## Consideraciones
+
+- Estoy usando .gitignore para no subir al repositorio la carpeta del entorno virtual (venv) y otros archivos temporales, dado que no sería buena práctiva.
+
+- El paquete flask lo instalo mediante archivo requeriments.txt, luego de la instalación utilicé el comando `pip freeze > requeriments-freeze.txt` para volcar la info de las versiones de las dependencias para no tener problemas de compatibilidad en futuras instalaciones.
+
+## Ejecutar
+
+1. Crear entorno virtual
+
+```shell
+python3 -m venv venv
+```
+
+2. Ingresar a entorno virtual
+
+```shell
+source venv/bin/activate
+```
+
+3. Instalar requisitos usando pip
+
+```shell
+pip install -r requeriments-freeze.txt
+```
+
+4. Levantar servicio
+
+```shell
+python3 app/main.py
+```
+
+5. Ingresar con un navegador a la página
+
+```
+http://localhost:5000/
+```
---
+++ app/main.py
@@ -0,0 +1,75 @@
+"""Fichero principal del proyecto flask.
+"""
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def inicio() -> str:
+    """Retorna el render de la página de inicio.
+
+    Returns:
+        str: Página renderizada.
+    """
+    return render_template("index.html")
+
+@app.route("/ejercicio1", methods=["GET", "POST"])
+def ejercicio1() -> str:
+    """Retorna el render de la página del ejercicio 1.
+    En caso de se llame como POST hará el calculo del promedio y lo mostrará
+    junto al renderizado.
+
+    Returns:
+        str: Página renderizada.
+    """
+    # inicializar variables
+    promedio = None
+    respuesta = None
+    formulario = ["", "", "", ""]
+    # si es post, que realice el calculo
+    if request.method == "POST":
+        # obtener valores del formulario
+        form = request.form
+        nota_1 = form["nota1"]
+        nota_2 = form["nota2"]
+        nota_3 = form["nota3"]
+        asistencia = form["asistencia"]
+        formulario = [nota_1, nota_2, nota_3, asistencia]
+        # validaciones
+        try:
+            # que sean numeros
+            nota_1 = int(nota_1)
+            nota_2 = int(nota_2)
+            nota_3 = int(nota_3)
+            asistencia = int(asistencia)
+            # que estén dentro de sus rangos correctos
+            if not (10 <= nota_1 <= 70): raise Exception("Nota fuera de rango")
+            if not (10 <= nota_2 <= 70): raise Exception("Nota fuera de rango")
+            if not (10 <= nota_3 <= 70): raise Exception("Nota fuera de rango")
+            if not (0 <= asistencia <= 100): raise Exception("Asistencia fuera de rango")
+        except:
+            # respuesta en caso que alguna validacion falle
+            respuesta = "Por favor valide que haya escrito correctamente los valores requeridos"
+        else:
+            # calcular promedio
+            promedio = (nota_1 + nota_2 + nota_3) / 3
+            if promedio >= 40 and asistencia >= 75:
+                respuesta = "APROBADO"
+            else:
+                respuesta = "REPROBADO"
+    # retornar template
+    return render_template("ejercicio1.html", promedio=promedio, respuesta=respuesta, formulario=formulario)
+
+@app.route("/ejercicio2")
+def ejercicio2() -> str:
+    """Retorna el render de la página del ejercicio 2.
+
+    Returns:
+        str: Página renderizada.
+    """
+    return render_template("ejercicio2.html")
+
+# Inicializar app
+if __name__ == '__main__':
+    app.run(debug=True)
